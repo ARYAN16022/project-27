cdb45bee5f11d36603e5f778613cd8dde6fbcc06
ARYAN16022 <84725640+ARYAN16022@users.noreply.github.com>
2021-09-17 22:55:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload() {}
+
+function setup() {
+  createCanvas(800, 700);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  roof1 = new roof(350,100, 300, 50);
+  bob1 = new bob(250, 300);
+  bob2 = new bob(300, 300);
+  bob3 = new bob(350, 300);
+  bob4 = new bob(400, 300);
+  bob5 = new bob(450, 300);
+
+  rope1 = new rope(bob1.body, roof1.body, -80, 0);
+  rope2 = new rope(bob2.body, roof1.body, -40, 0);
+  rope3 = new rope(bob3.body, roof1.body, 0, 0);
+  rope4 = new rope(bob4.body, roof1.body, 40, 0);
+  rope5 = new rope(bob5.body, roof1.body, 80, 0);
+  Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  roof1.display();
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  drawSprites();
+}
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    Matter.Body.applyForce(bob1.body, bob1.body.position, { x: -730, y: 0 });
+  }
+}
